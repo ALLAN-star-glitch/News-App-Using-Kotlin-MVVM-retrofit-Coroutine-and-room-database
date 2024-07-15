eca8ed780f41f4e779dbab57a4efff228739c9ea
Allan Mathenge <allanmathenge67@gmail.com>
2024-07-15 16:05:49 +0300

    added NewsAPI interface, data classes for the model package,constants, as well as Retrofit Instance

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val BASE_URL="https://newsapi.org/"
+        const val API_KEY="e8eac5660cca4647975c1c6b851225e8"
+        const val SEARCH_NEWS_TIME_DELAY="500L"
+        const val QUERY_PAGE="20"
+    }
+}
